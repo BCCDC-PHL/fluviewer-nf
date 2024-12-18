3fed7fd34ce542a9347e1d5eddf3e6e8f6db28e7
John Palmer <john.palmer@bccdc.ca>
2024-12-18 13:32:02 -0800

    updates to run_snpeff.py

--- bin/run_snpeff.py
+++ bin/run_snpeff.py
@@ -6,69 +6,9 @@ from typing import Tuple, List
 from functools import partial
 import gzip 
 from subprocess import run, CalledProcessError
-
+from tools import read_vcf, write_vcf
 
 SEGMENTS = 'PB2 PB1 PA HA NP NA M NS'.split()
-
-def read_vcf(file_path: str , max_header_lines=300) -> Tuple[pd.DataFrame, List[str]]:
-    """
-    Parse a Variant Call Format (VCF) file into a pandas DataFrame and header list.
-    
-    Args:
-        file_path: Path to the VCF file
-        
-    Returns:
-        Tuple containing:
-            - pandas DataFrame with VCF data
-            - List of header lines (including metadata)
-    """
-    # Store header lines
-    header_lines = []
-    
-    # Read header lines
-    read_function = partial(open, mode='r') if not file_path.endswith('.gz') else partial(gzip.open, mode='rt')
-
-    with read_function(file_path) as infile:
-        line_count = 0 
-        line = infile.readline()
-        while not line.lstrip('#').startswith("CHROM"):
-            line_count += 1
-            header_lines.append(line.strip())
-            line = infile.readline()
-
-            if line_count > max_header_lines:
-                print(f"ERROR: Exceeded {max_header_lines} lines to search for the CHROM start point.")
-                sys.exit(1)
-
-        header_lines.append(line.strip())
-        
-        column_names = header_lines[-1].lstrip("#").split("\t")
-
-        df = pd.read_csv(infile, sep='\t', names=column_names)
-    
-    return df, header_lines
-
-
-def write_vcf(df, header_lines, output_path):
-    """
-    Write out a Variant Call Format (VCF) file using a Pandas df, list of headers, and output path
-    
-    Args:
-        file_path: Path to the VCF file
-        
-    Returns:
-        Tuple containing:
-            - pandas DataFrame with VCF data
-            - List of header lines (including metadata)
-    """
-
-    # Read header lines
-    with open(output_path, 'w') as outfile:
-        for line in header_lines:
-            outfile.write(line + '\n')
-
-        df.to_csv(outfile, sep='\t', index=False, header=False)
-
 
 def rename_vcf(df):
     df['CHROM'] = df['CHROM'].str.split("|").str[-1]
@@ -140,11 +80,13 @@ def main():
         
         annotated_vcfs[segment] = output_vcf
     
+    _ , ann_header_lines = read_vcf('.tmp.vcf')
+
     final_vcf_df = concat_vcf(annotated_vcfs)
 
     final_vcf_path = f"{args.outname}_all_anno.vcf"
     print(f'Writing combined VCF to {final_vcf_path}')
-    write_vcf(final_vcf_df, header_lines, final_vcf_path)
+    write_vcf(final_vcf_df, ann_header_lines, final_vcf_path)
 
 
 if __name__ == '__main__':
